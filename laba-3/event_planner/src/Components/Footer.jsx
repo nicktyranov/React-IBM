63692e7a8bc22a0c99d9f16913c68bafb1108cf5
Nick Tyranov <nicktyranov@gmail.com>
2025-02-04 17:17:10 -0700

    laba-3

---
+++ laba-3/event_planner/src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+const Footer = () => {
+	return (
+		<>
+			<footer>
+				<p>{new Date().getFullYear()}&copy; Event Planner Organization. All rights reserved.</p>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
